189665984228f34ef2d0dc94e73f666cb96a603b
DS <dmanstark@gmail.com>
2025-01-17 18:06:45 -0700

    modularized the intraday and daily datagrabs. Probably need an action for each thing we want to do as the api has different keys

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,41 @@
-import { testing_api } from "@/actions/basic-data-grab";
+import { StockDataGrabIntraday } from "@/lib/actions/general-datagrab_intraday";
 import { BasicLineChart } from "@/components/ui/basic-line-chart";
 import { NextPage } from "next";
 import Image from "next/image";
+import { StockDataGrabDaily } from "@/lib/actions/general-datagrab_daily";
 
 const Home:React.FC = async () => {
 
-  let value = await testing_api()
+  let ibmIntraday60MinData = await StockDataGrabIntraday(
+    {
+      function: 'TIME_SERIES_INTRADAY',
+      interval: '5min',
+      symbol: 'IBM',
+    }
+  )
+
+  let ibmDailyData = await StockDataGrabDaily(
+    {
+      function: 'TIME_SERIES_DAILY',
+      symbol: 'IBM',
+    }
+  )
+
+  let tscoLonDailyData = await StockDataGrabDaily(
+    {
+      function: 'TIME_SERIES_DAILY',
+      symbol: 'IBM',
+      outputsize: 'full'
+    }
+  )
 
 
   return (
       <main className="flex grow">
         <div className="flex flex-col grow pt-8 pr-8 min-w-full ">
-          <BasicLineChart chartData={value}/>
+          <BasicLineChart chartData={ibmIntraday60MinData}/>
+          <BasicLineChart chartData={ibmDailyData}/>
+          <BasicLineChart chartData={tscoLonDailyData}/>
         </div>
       </main>
   );
---
+++ lib/actions/general-datagrab_daily.ts
@@ -0,0 +1,85 @@
+'server only'
+
+import fetch from 'node-fetch';
+import { env } from 'process';
+
+type FunctionLabelFromApi = {
+    [index: string]: Record<string, BaseDataEntry>
+}
+
+type MetaData = {
+    'Meta Data': Object,
+}
+
+type BaseData = FunctionLabelFromApi & MetaData
+
+type BaseDataEntry = {
+    '1. open': string,
+    '2. high': string,
+    '3. low': string,
+    '4. close': string,
+    '5. volume': string,
+}
+
+type BaseReturnData = {
+    open: Number,
+    high: Number,
+    low: Number,
+    close: Number,
+    volume: Number,
+}
+
+export type ReturnDataDaily = {
+    'date': string,
+} & BaseReturnData
+
+//grabbed from alphavantage's site here:
+//https://www.alphavantage.co/documentation/#intraday
+export type DailyDataGrabParams = {
+    function: string,
+    symbol: string,
+    outputsize?: string,
+}
+
+export const StockDataGrabDaily = async (params: DailyDataGrabParams) => {
+
+    // let apiKey = process.env.ALPHA_VANTAGE_API_KEY
+
+    let apiKey = 'demo'
+
+    let paramsOuputSize = params.outputsize ? `&outputsize=${params.outputsize}` : ``
+    
+    var url = `https://www.alphavantage.co/query?function=${params.function}&symbol=${params.symbol}` + paramsOuputSize + `&apikey=${apiKey}`;
+
+    console.log(url)
+    
+    const response = await fetch(url);
+    let data = await response.json() as BaseData;
+
+    console.log(data)
+    let returnData = [] as ReturnDataDaily[]
+
+    Object.keys(data['Time Series (Daily)']).forEach((key) => {
+
+        let entryData = data['Time Series (Daily)'][key]
+
+        let pushObj = {
+            date: key,
+            open: parseFloat(entryData['1. open']),
+            high: parseFloat(entryData['2. high']),
+            low: parseFloat(entryData['3. low']),
+            close: parseFloat(entryData['4. close']),
+            volume: parseFloat(entryData['5. volume']),
+        } as ReturnDataDaily
+
+        
+
+        returnData.push(pushObj)
+    }) 
+
+    returnData.sort((a,b) => {
+        return(a.date.localeCompare(b.date))
+    })
+
+    return(returnData)
+}
